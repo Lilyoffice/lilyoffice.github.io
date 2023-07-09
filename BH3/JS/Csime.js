a96153829a41b24b9e5a7bcf3e536108a6668553
Lilyoffice <138017572+Lilyoffice@users.noreply.github.com>
2023-07-09 20:53:43 +0800

    Update Csime.js

--- BH3/JS/Csime.js
+++ BH3/JS/Csime.js
@@ -8,7 +8,7 @@
 */
 function calculate() {
     
-    var days = parseInt(document.getElementById("days").value); 
+    var days = parseInt(document.getElementById("days").value);      // 获取时间内容
     var BoolCard = parseInt(document.getElementById("Cards").value); // 月卡是否开启：1 - 开启 0 = 不开启
     if(isNaN(days)){
        days = 0;
@@ -36,35 +36,39 @@ function calculate() {
   
     // 计算模块
     // 月卡计算
+
+//  Dubug : 月卡错误 终止程序
     if (BoolCard < 0 || BoolCard > 1 || isNaN(BoolCard)) {
       document.getElementById("maxs").innerHTML ="输入的月卡开启内容有误，请重试";
       document.getElementById("maxs").innerHTML ="&nbsp;"
       return;
     }
-    var CryCard = BoolCard == 1 ? (days * DailyCard + parseInt(days / 15) * CardXV + parseInt(days / 30) * BuyCard) : 0;
+    var CryCard = BoolCard == 1 ? (days * DailyCard + parseInt(days / 15) * CardXV + parseInt(days / 30) * BuyCard) : 0; // 计算月卡 -单独计算-
     var Crys = 0;
   
     // 深渊计算和签到计算
     var weeks = parseInt(days/7);
-    var ODD = days - weeks * 7;
-    if (days <= 14){
+    var ODD = days - weeks * 7; // ODD是多余的天数
+    if (days <= 14){ //如果日期小于14，活动奖励按0计算
       HD = 0;
     }
-    if (ODD >= 4){
+    if (ODD >= 4){ // 签到 + 深渊
       Crys = Abyss + qiandao;
     }
   
     var VerODDmax = parseInt(days / BigUpDay);
     var VerODDmin = parseInt(days / MinUpDay);
-    var maxCRY = Crys + daily * days + weeks * (Abyss * 2 + ZC + qiandao * 2) + CryCard + VerODDmax * (QA + Live + Update) + HD * (VerODDmax + 1);
-    var minCRY = Crys + daily * days + weeks * (Abyss * 2 + ZC + qiandao * 2) + CryCard + VerODDmin * (QA + Live + Update) + HD * (VerODDmin + 1);
+    var maxCRY = Crys + daily * days + weeks * (Abyss * 2 + ZC + qiandao * 2) + CryCard + VerODDmax * (QA + Live + Update) + HD * (VerODDmax + 1); // 大版本计算
+    var minCRY = Crys + daily * days + weeks * (Abyss * 2 + ZC + qiandao * 2) + CryCard + VerODDmin * (QA + Live + Update) + HD * (VerODDmin + 1); // 小版本计算
+    
+	// 内容显示
   
     document.getElementById("maxs").innerHTML =  "大版本(42天)获取的水晶为: "  + maxCRY;
     document.getElementById("mins").innerHTML =  "小版本(35天)获取的水晶为: "  + minCRY;
 }
 
 
-function clearInput() {
+function clearInput() {  // 清屏 cls
     document.getElementById("days").value = "";
 	document.getElementById("Cards").value = "";
     document.getElementById("maxs").innerHTML = "&nbsp;";
