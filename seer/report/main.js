d01aade909a5d2ff0b9fdfeac4ae85fff2f613d3
Lilyoffice <138017572+Lilyoffice@users.noreply.github.com>
2023-07-17 20:25:00 +0800

    新功能 赛尔报盘

---
+++ seer/report/main.js
@@ -0,0 +1,29 @@
+window.onload = function() {
+    // 使用 AJAX 加载外部 XML 文件
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "main.xml", true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            // 解析 XML 文件
+            var xmlDoc = xhr.responseXML;
+            var students = xmlDoc.getElementsByTagName("student");
+
+            // 填充表格内容
+            var tableBody = document.querySelector("#myTable tbody");
+            for (var i = 0; i < students.length; i++) {
+                var student = students[i];
+                var id = student.getElementsByTagName("id")[0].textContent;
+                var name = student.getElementsByTagName("name")[0].textContent;
+                var naI = student.getElementsByTagName("natureI")[0].textContent;
+                var naII = student.getElementsByTagName("natureII")[0].textContent;
+
+                var row = tableBody.insertRow();
+                row.insertCell().textContent = id;
+                row.insertCell().textContent = name;
+                row.insertCell().textContent = naI;
+                row.insertCell().textContent = naII;
+            }
+        }
+    };
+    xhr.send();
+};
