16b7c51d40fb4c4b46fd9f0d8b21e1462d2f821a
Lilyoffice <138017572+Lilyoffice@users.noreply.github.com>
2023-07-23 17:18:27 +0800

    Update mianpm.js

--- pokemon/save/mianpm.js
+++ pokemon/save/mianpm.js
@@ -1,4 +1,23 @@
 window.onload = function() {
+        var SaveTime = "2023/7/22 晚"//存档时间
+       document.getElementById("Time").innerHTML=SaveTime
+
+       var SaveName = "陆猛"//存档时间
+       document.getElementById("Name").innerHTML=SaveName
+
+       var SaveOtherTrainName = "双笙、嘉德丽雅、卡芙卡"//同行人
+       document.getElementById("OName").innerHTML=SaveOtherTrainName
+
+// 以下内容注意修改
+
+       var Area = "合众地区"//所在地区
+       var City = "三耀市"//所在位置
+       var HZ = 1 //徽章数
+       document.getElementById("Area").innerHTML=Area
+       document.getElementById("City").innerHTML=City
+       document.getElementById("HZ").innerHTML=HZ
+
+    //以下内容为读取XML，不要改
     var xhr = new XMLHttpRequest();
     xhr.open("GET", "main.xml?ver=230722", true);//注意更新版本号
     xhr.onreadystatechange = function() {
