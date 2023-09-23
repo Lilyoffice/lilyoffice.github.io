1ece7bcd52c43126d2c6300172d999ec8f89e261
Lilyoffice <138017572+Lilyoffice@users.noreply.github.com>
2023-09-23 19:42:13 +0800

    Update main.js

--- seer/report/main.js
+++ seer/report/main.js
@@ -1,4 +1,5 @@
 window.onload = function() {
+    // 230923 标准化用语
     // 使用 AJAX 加载外部 XML 文件
     var xhr = new XMLHttpRequest();
     xhr.open("GET", "main.xml?ver=18A&Form=JS113Lily", true);//注意更新版本号
@@ -6,33 +7,33 @@ window.onload = function() {
         if (xhr.readyState === 4 && xhr.status === 200) {
             // 解析 XML 文件
             var xmlDoc = xhr.responseXML;
-            var students = xmlDoc.getElementsByTagName("pm");
+            var Pms = xmlDoc.getElementsByTagName("pm");
 
             // 填充表格内容
             var tableBody = document.querySelector("#myTable tbody");
-            for (var i = 0; i < students.length; i++) {
-                var student = students[i];
+            for (var i = 0; i < Pms.length; i++) {
+                var Pokemon = Pms[i];
                 //精灵基础信息
-                var id = student.getElementsByTagName("id")[0].textContent;
-                var name = student.getElementsByTagName("name")[0].textContent;
-                var naI = student.getElementsByTagName("natureI")[0].textContent;
-                var naII = student.getElementsByTagName("natureII")[0].textContent;
+                var id = Pokemon.getElementsByTagName("id")[0].textContent;
+                var name = Pokemon.getElementsByTagName("name")[0].textContent;
+                var naI = Pokemon.getElementsByTagName("natureI")[0].textContent;
+                var naII = Pokemon.getElementsByTagName("natureII")[0].textContent;
                 //精灵养成信息
-                var race = student.getElementsByTagName("race")[0].textContent;
-                var level = student.getElementsByTagName("level")[0].textContent;
-                var ind = student.getElementsByTagName("ind")[0].textContent;
-                var cha = student.getElementsByTagName("cha")[0].textContent;
-                var chara = student.getElementsByTagName("chara")[0].textContent;
+                var race = Pokemon.getElementsByTagName("race")[0].textContent;
+                var level = Pokemon.getElementsByTagName("level")[0].textContent;
+                var ind = Pokemon.getElementsByTagName("ind")[0].textContent;
+                var cha = Pokemon.getElementsByTagName("cha")[0].textContent;
+                var chara = Pokemon.getElementsByTagName("chara")[0].textContent;
                     //精灵学习力 <Adt><Adf><Apt><Apf><spd><hp>
-                    var Adt = student.getElementsByTagName("Adt")[0].textContent;
-                    var Adf = student.getElementsByTagName("Adf")[0].textContent;
-                    var Apt = student.getElementsByTagName("Apt")[0].textContent;
-                    var Apf = student.getElementsByTagName("Apf")[0].textContent;
-                    var Spd = student.getElementsByTagName("Spd")[0].textContent;
-                    var HP = student.getElementsByTagName("HP")[0].textContent;
+                    var Adt = Pokemon.getElementsByTagName("Adt")[0].textContent;
+                    var Adf = Pokemon.getElementsByTagName("Adf")[0].textContent;
+                    var Apt = Pokemon.getElementsByTagName("Apt")[0].textContent;
+                    var Apf = Pokemon.getElementsByTagName("Apf")[0].textContent;
+                    var Spd = Pokemon.getElementsByTagName("Spd")[0].textContent;
+                    var HP = Pokemon.getElementsByTagName("HP")[0].textContent;
                     var zzz = parseInt(Adt) + parseInt(Adf) + parseInt(Apt) + parseInt(Apf) + parseInt(Spd) + parseInt(HP)
                 //精灵标识相关 巡礼部 20230718
-                var pmlabel = student.getElementsByTagName("pmlabel")[0].textContent;
+                var pmlabel = Pokemon.getElementsByTagName("pmlabel")[0].textContent;
                 
 
                 
